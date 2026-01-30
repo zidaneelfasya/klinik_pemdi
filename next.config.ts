b9f3d2b1a5240c3ceda68fb1bcd19271ed0677dd
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2026-01-30 10:37:03 +0700

    fix: remove reactCompiler from experimental for Next 16

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	experimental: {
-		reactCompiler: false,
-	},
+
 };
 
 export default nextConfig;
